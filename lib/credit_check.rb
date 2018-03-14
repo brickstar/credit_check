2924eaf2ca5a293926fd4e038a6b591dbfb7d846
brickstar <brickstar@gmail.com>
2018-03-13 22:40:40 -0600

    working, needs validate test

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,14 +1,10 @@
 class CreditCheck
-
-  def initialize(account_identifier)
-    @account_identifier = account_identifier
-    @results = 0
-  end
-
-  def convert_string
-    string.split('').map do |num|
-      num.to_i
-    end.chars
+  attr_reader :digits,
+              :check_digit
+              
+  def initialize(card_number)
+    @digits = card_number.to_i.digits
+    # @check_digit = @digits.pop
   end
 
   def two_times_every_other_digit
@@ -21,53 +17,37 @@ class CreditCheck
     end
   end
 
-  def sum_greater_than_ten(array)
-    results = array.map.with_index do |num, index|
+  def sum_digits_over_ten
+    @digits.map!.with_index do |num, index|
       if num > 9
         num - 9
       else
         num
       end
     end
-    @results = results.sum
   end
 
-  def valid?
-    if @results % 10 == 0
-      puts "Card number #{@card_number} is valid!"
-    else
-      puts "Card number #{@card_number} is invalid!"
-    end
-  end
-
-  def check_number(number)
-
-  end
-
-
-  def check_number
-
-  end
-
-end
-
-
-
-class CreditCheck
-
-  def initialize(card_number)
-    @digits = card_number.to_i.digits
-    @check_sum = @digits.pop
+  def results_summed
+    @digits = @digits.sum
   end
 
   def validate
-    "The number is valid!"
+    two_times_every_other_digit
+    sum_digits_over_ten
+    results_summed
+    if @digits % 10 == 0
+      "The number is valid!"
+    else
+      "The number is invalid!"
+    end
     # Account identifier:    7   9   9   2   7   3   9   8   7   1   3
     # 2x every other digit:  7   18  9   4   7   6   9   16  7   2   3
     # Summed digits over 10: 7   9   9   4   7   6   9   7   7   2   3
     # Results summed:        7   9   9   4   7   6   9   7   7   2   3 = 70
     # valid if sum is evenly divisible by 10
   end
+
+
 
 
 
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -5,23 +5,37 @@ require 'pry'
 
 class CreditCheckTest < Minitest::Test
 
-  def test_returns_valid
-    valid = ['5541801923795240', '4024007136512380', '6011797668867828']
-    expected = "The number is valid!"
-
-    valid.each { |card_number| assert_equal expected, CreditCheck.new(card_number).validate }
+  def test_it_exists
+    cc = CreditCheck.new("5541808923795240")
+binding.pry
+    assert_instance_of CreditCheck, cc
   end
 
-  def test_returns_invalid
-    invalid = ['5541801923795240', '4024007106512380', '6011797668868728']
-    expected = "The number is invalid!"
+  def test_digits
+    cc = CreditCheck.new("5541808923795240")
 
-    invalid.each { |card_number| assert_equal expected, CreditCheck.new(card_number).validate }
+    assert_equal [5, 5, 4, 1, 8, 0, 8, 9, 2, 3, 7, 9, 5, 2, 4], cc.digits
   end
 
-#private methods
-  def test_converts_string_to_digits
+  def test_check_digit
+    cc = CreditCheck.new("5541808923795240")
 
-    asset_equal [0, 4, 2, 5, 9, 7, 3, 2, 9, 1, 0, 8, 1, 4, 5, 5],
+    assert_equal 0, cc.check_digit
+  end
+
+  def test_two_times_every_other_digit
+    cc = CreditCheck.new("5541808923795240")
+
+    assert_equal [5, 10, 4, 2, 8, 0, 8, 18, 2, 6, 7, 18, 5, 4, 4], cc.two_times_every_other_digit
+  end
+
+  def test_sum_digits_over_ten
+    cc = CreditCheck.new("5541808923795240")
+
+    assert_equal 72, cc.sum_digits_over_ten
+  end
+
+  def test_validate
+
   end
 end
